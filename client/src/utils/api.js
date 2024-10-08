b2c7852af04b736add4f7b5e85122963fead336b
Lino Khan <Lino Khan>
2024-10-08 13:59:16 +0200

    added client directory as base in netlify.toml

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,11 +2,17 @@ const API_URL = import.meta.env.VITE_BASE_API_URL;
 
 export const fetchPosts = async () => {
     try {
+        console.log(`Fetching posts from: ${API_URL}/getPosts`);
         const response = await fetch(`${API_URL}/getPosts`);
+        
         if (!response.ok) {
+            console.error(`Error fetching posts: ${response.status} - ${response.statusText}`);
             throw new Error('Network response was not ok');
         }
-        return await response.json();
+
+        const data = await response.json();
+        console.log("Fetched posts successfully:", data);
+        return data;
     } catch (error) {
         console.error("Failed to fetch posts:", error);
         throw error;
